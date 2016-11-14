0c2c44581e4229d292cc5666cff6d75d93057f85
پارسا غفوري <telegram.bots2@gmail.com>
2016-11-14 13:55:56 +0330

    Create crector.lua

---
+++ plugins/crector.lua
@@ -0,0 +1,24 @@
+do
+
+function run(msg, matches)
+  return [[crector is
+  parsa ghafoori.s
+  iKD Comapany
+  =================
+  @parsaghafoori
+  @parsa_masagerbot
+  @zatabot
+  @anti_spam_group]
+
+end
+
+return {
+  description = " bot cerector
+  usage = "crector
+  patterns = {
+    "^[!/#]cerector$"
+  }, 
+  run = run 
+}
+
+end
