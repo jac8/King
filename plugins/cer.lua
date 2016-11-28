9bf7534f4ccf8ed4c6f6c9b8bbf5c7174a81e48f
پارسا غفوري <telegram.bots2@gmail.com>
2016-11-28 17:35:42 +0330

    Update cer.lua

--- plugins/cer.lua
+++ plugins/cer.lua
@@ -2,6 +2,10 @@
 local function run(msg, matches)
 return [[سازنده این روبات،
 پارسا غفوری است!
+  نام سورس:
+  king
+ 
+  iKD Company™
 @parsaghafoori
 @anti_spam_group]]
-end return {patterns = {"^!cer$"},run = run}
+end return {patterns = {"^king$"},run = run}
