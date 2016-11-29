0a5cc365c86b3a5d26216b3461f6834ebee5b855
پارسا غفوري <telegram.bots2@gmail.com>
2016-11-29 14:11:35 +0330

    Update keep2.lua

--- plugins/keep2.lua
+++ plugins/keep2.lua
@@ -2,7 +2,7 @@
 		local text1 = matches[2]
         local text2 = matches[3]
 		local url =
-"http://www.myfunstudio.com/designs/glt/make?t=Game%0D%0ALetter%0D%0ATiles&ts=1&a=z&s=Classic&l=en&b=d3&c_tb=302209&c_tf=E6ED1F&c_tt=030708&c_nt=FFFFFF&c_d3=54340A&c_bc=41992B&w=500&name=text1&caption=text2&action_ProcessFrm=Update+products"
+""
 		 local  file = download_to_file(url,'keep.webp')
 			send_document(get_receiver(msg), file, ok_cb, false)
 
